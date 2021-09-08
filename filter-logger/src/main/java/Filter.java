84c70e2241d09adbf22aff290acda4d0820690f2
Alexander Lukyanov <lux-nova@mail.ru>
2021-09-09 02:33:38 +0300

    logger has it

--- filter-logger/src/main/java/Filter.java
+++ filter-logger/src/main/java/Filter.java
@@ -11,6 +11,7 @@ public class Filter {
     public List<Integer> filterOut(List<Integer> source) {
         Logger logger = Logger.getInstance();
         List<Integer> result = new ArrayList<>();
+        logger.log("Начинаем фильтрацию");
         for (Integer i : source) {
             if (i < threshold) {
                 logger.log("Число \"" + i + "\" не проходит");
@@ -19,6 +20,7 @@ public class Filter {
                 logger.log("Число \"" + i + "\" проходит");
             }
         }
+        logger.log("Рубеж фильтрации преодолели %d чисел из %d".formatted(result.size(), source.size()));
         return result;
     }
 }
--- filter-logger/src/main/java/Main.java
+++ filter-logger/src/main/java/Main.java
@@ -1,51 +1,88 @@
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static int N, M, f;
-    static ArrayList<Integer> list;
+    static List<Integer> list;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Logger logger = Logger.getInstance();
-        System.out.println("Здравствуйте! Ответьте на несколько нетрудных вопросов!");
         logger.log("Здороваемся с пользователем");
-        System.out.println("Сколько будет чисел в нашем списке?");
+        System.out.println("Здравствуйте! Ответьте на несколько нетрудных вопросов!");
         logger.log("Запрашиваем величину списка");
         while (true) {
+            System.out.println("Сколько будет чисел в нашем списке?");
             try {
                 N = scanner.nextInt();
             } catch (InputMismatchException e) {
-                System.out.println("Неправильный ввод!");
                 logger.log("Юзер ввёл число в неверном формате");
+                System.out.println("Неправильный ввод!");
+                scanner = new Scanner(System.in);
                 continue;
             }
             if (N < 1) {
-                System.out.println("Количество должно быть положительным целым числом!");
                 logger.log("Юзер ввёл неположительное число");
+                System.out.println("Количество должно быть положительным целым числом!");
                 continue;
             }
             logger.log("Получена длина списка: " + N);
             break;
         }
-        System.out.println("Каким будет наибольшее возможное число в нашем списке?");
         logger.log("Запрашиваем ограничение на максимальное значение");
         while (true) {
+            System.out.println("Каким будет наибольшее возможное число в нашем списке?");
             try {
                 M = scanner.nextInt();
             } catch (InputMismatchException e) {
-                System.out.println("Неправильный ввод!");
                 logger.log("Юзер ввёл число в неверном формате");
+                System.out.println("Неправильный ввод!");
+                scanner = new Scanner(System.in);
                 continue;
             }
             if (M < 1) {
-                System.out.println("Это должно быть положительное целое число!");
                 logger.log("Юзер ввёл слишком маленькое число");
+                System.out.println("Это должно быть положительное целое число!");
                 continue;
             }
             logger.log("Получено пороговое значение для генерации: " + M);
             break;
         }
 
+        list = new ArrayList<>(N);
+        Random random = new Random();
+        for (int i = 0; i < N; i++)
+            list.add( random.nextInt(M + 1));
+        logger.log("Сгенерировали список по заданным параметрам");
+        System.out.print("Вот случайный список:");
+        for (Integer i : list) System.out.print( " " + i );
+        System.out.println();
+
+        logger.log("Запрашиваем порог фильтрации");
+        while (true) {
+            System.out.println("Меньшие какого числа мы теперь исключим значения?");
+            try {
+                f = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                logger.log("Юзер ввёл число в неверном формате");
+                System.out.println("Неправильный ввод!");
+                scanner = new Scanner(System.in);
+                continue;
+            }
+            if (f < 1 || f > M) {
+                logger.log("Юзер ввёл некорректное значение порога");
+                System.out.println("Это должно быть число в рамках диапазона!");
+                continue;
+            }
+            logger.log("Получено пороговое значение для фильтрации: " + f);
+            break;
+        }
+        Filter filter = new Filter(f);
+        List<Integer> filteredList = filter.filterOut(list);
+
+        logger.log("Выводим результат");
+        System.out.print("Вот отфильтрованный список:");
+        for (Integer i : filteredList) System.out.print( " " + i );
+        System.out.println();
+        logger.log("Завершаем работу программы");
+        System.out.println("Спасибо за работу!");
     }
 }
